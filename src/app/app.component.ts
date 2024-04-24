d4ecba0c65570fbcab0e2c1b8674eb1ce08ff9a0
Christian <Christian>
2024-04-24 20:04:53 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserCardComponentComponent } from './components/user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserCardComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'component-crafts';
---
+++ src/app/components/user-card/user-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-user-card',
+  templateUrl: 'user-card.component.html',
+  styleUrl: 'user-card.component.scss',
+})
+export class UserCardComponentComponent {}
